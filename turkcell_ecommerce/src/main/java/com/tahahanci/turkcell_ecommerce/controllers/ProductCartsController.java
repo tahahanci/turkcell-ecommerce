1644810553b62074e03a2d932b0cbe14154eb7ff
Ersel Celal Eren <erseleren_@hotmail.com>
2024-03-09 13:11:14 +0300

    ProductCarts @RestController Annotation eksikliği giderildi.

--- turkcell_ecommerce/src/main/java/com/tahahanci/turkcell_ecommerce/controllers/ProductCartsController.java
+++ turkcell_ecommerce/src/main/java/com/tahahanci/turkcell_ecommerce/controllers/ProductCartsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/api/productCarts")
 public class ProductCartsController {
 
