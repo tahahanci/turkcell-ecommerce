3d07efcaad9b940992e8ac40d5255fe0198c912b
Taha Hancı <taha.hanci@boun.edu.tr>
2024-03-14 22:50:17 +0300

    ProductSupplierListResponse dto created.

---
+++ turkcell_ecommerce/src/main/java/com/tahahanci/turkcell_ecommerce/services/dtos/productsupplier/response/ProductSupplierListResponse.java
@@ -0,0 +1,12 @@
+package com.tahahanci.turkcell_ecommerce.services.dtos.productsupplier.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductSupplierListResponse {
+
+    private String supplierName;
+    private String productName;
+}
